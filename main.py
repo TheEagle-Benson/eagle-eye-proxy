56d04383d4c85c735a7d4ce40801a1ba9acd77d4
The Eagle <theeaglebenson@gmail.com>
2025-12-09 15:28:45 +0000

    list origins to allow request from

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import httpx
 import os
+
+load_dotenv()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://eagle-eye-navigation.netlify.app"
+]
+api_key = os.getenv("API_KEY")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_headers=["*"]
+)
